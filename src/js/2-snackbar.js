da2114e5ad6ecc33f695f6d5ea6ee7ef98ca0848
tetiana_hushnovska <thushnyovska@gmail.com>
2025-03-02 16:31:07 +0000

    add js file

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,37 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+document.querySelector('form').addEventListener('submit', event => {
+    event.preventDefault();
+
+    const delay = parseInt(event.target.delay.value);
+    const state = event.target.state.value;
+
+    createPromise(delay, state)
+        .then(delay => {
+            iziToast.success({
+                title: '✅',
+                message: `Fulfilled promise in ${delay}ms`,
+            });
+        })
+        .catch(delay => {
+            iziToast.error({
+                title: '❌',
+                message: `Rejected promise in ${delay}ms`,
+            });
+        });
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        console.log(`Promise ${state} in ${delay} ms`);
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
+
